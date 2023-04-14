8239c226c51459a3b90374d312d890cc70ada89f
Adam Bezecny <adam.bezecny@sporticon.cz>
2023-04-14 09:14:32 +0200

    feat(sample-eks-cluster): added basic aws provider config

---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "aws_region" {
+  description = "Region in which AWS Resources will be created"
+  type        = string
+  default     = "eu-central-1"
+}
---
+++ versions.tf
@@ -0,0 +1,16 @@
+# Terraform Settings Block
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Terraform Provider Block
+provider "aws" {
+  region  = var.aws_region
+  profile = "eks_mtp"
+}
